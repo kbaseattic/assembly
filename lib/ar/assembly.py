6e3c0adb04ce1d8b12845cb40af2734f57bc716d
Christopher Bun <chrisbun@gmail.com>
2012-07-24 13:20:12 -0500

    assembly.py

---
+++ lib/ar/assembly.py
@@ -0,0 +1,17 @@
+"""
+Assembly execution library
+"""
+
+def run(assemblers):
+    """ Iterate assemblers(list) and run assembly jobs
+    """
+    return 2
+
+def run_kiki():
+    return 2
+
+def run_velvet():
+    return 2
+
+def run_soapdenovo():
+    return 2
