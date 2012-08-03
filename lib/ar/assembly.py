9c0c1311e47f40448a6d89a8c7aaa9c2ff49e5f3
Christopher Bun <chrisbun@gmail.com>
2012-08-03 21:25:18 +0000

    consumer: run velvet

--- lib/ar/assembly.py
+++ lib/ar/assembly.py
@@ -1,17 +1,76 @@
 """
 Assembly execution library
 """
+import os
+import subprocess
 
-def run(assemblers):
-    """ Iterate assemblers(list) and run assembly jobs
+## Available Assemblers ##
+assemblers = [
+{
+        'name' : 'Kiki',
+        'aliases' : ['kiki', 'ki'],
+        'command' : 'kiki'
+},
+{
+        'name' : 'Velvet',
+        'aliases' : ['velvet',],
+        'command' : 'velvet'
+},
+{
+        'name' : 'SOAPdenovo',
+        'aliases' : ['soap'],
+        'command' : 'soapdenovo'
+}
+]
+
+velvet_defaults = {
+    'output_dir' : 'auto',
+    'hash_length' : '31,45,2',
+    'file_type' : '-fasta'
+}
+
+
+def is_available(assembler):
+    """ Check if ASSEMBLER is a valid/available assembler
     """
-    return 2
+    return True
+
+def run(assembler, datapath):
+    if is_available(assembler):
+        if assembler == 'kiki':
+            print "Starting kiki"
+            run_kiki(datapath)
+        elif assembler == 'velvet':
+    
+            run_velvet(datapath)
 
 def run_kiki():
+
+
+    # Return location of finished data
     return 2
 
-def run_velvet():
-    return 2
+
+def run_velvet(datapath):
+    velvet_data = datapath 
+    velvet_data += '/velvet/'
+    os.makedirs(velvet_data)
+
+    # Set up parameters
+    hash = velvet_defaults['hash_length']
+    file_type = velvet_defaults['file_type']
+
+    # Run velvet
+    print "Starting velvet"
+    args = ['/usr/bin/velveth',
+            velvet_data,
+            hash,
+            file_type]
+    raw_path = datapath + '/raw/'
+    for file in os.listdir(raw_path):
+        readfile = raw_path + file
+        args.append(readfile)
+    p = subprocess.Popen(args)
 
 def run_soapdenovo():
     return 2
