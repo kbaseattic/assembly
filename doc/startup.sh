40f3181f6413586710c5742d10c5d6cabbd880da
Christopher Bun <chrisbun@gmail.com>
2012-08-15 23:02:56 +0000

    remove apt upgrade

--- doc/startup.sh
+++ doc/startup.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dependencies..."
 sudo apt-get -y update
-sudo apt-get -y upgrade
+#sudo apt-get -y upgrade
 sudo apt-get -y install python-nova build-essential python-pip rabbitmq-server git mongodb
 sudo pip install pika
 sudo pip install python-daemon
