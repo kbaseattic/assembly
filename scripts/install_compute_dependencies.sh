5b7384180ad78128c6b8059ceaf88a0077f638c7
Christopher Bun <chrisbun@gmail.com>
2012-09-07 19:02:04 +0000

    compute dependencies

---
+++ scripts/install_compute_dependencies.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get -y install python-nova build-essential python-pip rabbitmq-server git mongodb cmake zlib1g-dev mpich2
+sudo pip install pika
+sudo pip install python-daemon
+sudo pip install pymongo
+sudo pip install requests
+sudo pip install --upgrade python-novaclient
+
+pushd /tmp/
+sudo git clone git://github.com/dzerbino/velvet.git
+cd velvet
+sudo make ’CATEGORIES=9’ ’MAXKMERLENGTH=99’ ’LONGSEQUENCES=1’ ’OPENMP=1’
+sudo cp velveth /usr/bin
+sudo cp velvetg /usr/bin
+
+cd ..
+sudo git clone git://github.com/GeneAssembly/kiki.git
+cd kiki
+sudo mkdir bin
+cd bin
+sudo cmake ..
+sudo make ki
+cp ki /usr/bin
