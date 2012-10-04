03983181de607bcb0897027f07f73879cd536711
Fangfang Xia <fangfang@mcs.anl.gov>
2012-10-04 06:33:12 +0000

    add client test

---
+++ kbase_test/test_arast_client.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+export ARASTURL=140.221.84.124
+export PATH=/kb/deployment/bin:$PATH
+
+TEST_DIR="test_${$}"
+
+function message {
+    echo
+    echo
+    echo
+    echo
+    echo "##########################"
+    echo "#"
+    echo "# $1"
+    echo "#"
+    echo "##########################"
+    echo
+    echo
+    echo
+    echo
+}
+
+
+message "Check queue status"
+arast status
+
+message "Tests Complete: PASSED"
