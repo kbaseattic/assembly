7601bf76f76517515da7136b1bd611cbcb22a132
Fangfang Xia <fangfang@mcs.anl.gov>
2012-10-04 06:47:11 +0000

    minor update

--- kbase_test/test_arast_client.sh
+++ kbase_test/test_arast_client.sh
@@ -27,4 +27,22 @@ function message {
 message "Check queue status"
 arast -s $ARASTURL stat
 
+message "Download synthetic metagenome (200MB)"
+curl -OL http://www.mcs.anl.gov/~fangfang/test/smg.fa
+
+message "Submit synthetic  metagenome for kiki assembly and bwa mapping validation"
+arast -s $ARASTURL run -a kiki -f smg.fa -bwa
+
+message "Check job status"
+sleep 5
+arast -s $ARASTURL stat
+
+message "Check random data status"
+sleep 5
+arast -s $ARASTURL stat --data 1
+
+message "Wait for job to finish and download results"
+sleep 60
+arast -s $ARASTURL get
+
 message "Tests Complete: PASSED"
