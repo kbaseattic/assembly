4f371811646ac83f7538493ced4548ff14e29f4c
Christopher Bun <chrisbun@gmail.com>
2013-02-04 21:06:58 +0000

    moved libraries

--- lib/compute_startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# This file is to be run by cloud-init when the cloud monitor launches a new instance
-
-sudo su
-cd /home/ubuntu/assembly/lib/
-git checkout cbun_dev
-git pull origin cbun_dev
-
-
