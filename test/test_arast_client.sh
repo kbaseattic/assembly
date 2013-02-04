01e2989e8dd253aa89c8b72042bfa855586f7ad7
Fangfang Xia <fangfang.xia@gmail.com>
2013-02-04 09:09:17 +0000

    minor update

--- test/test_arast_client.sh
+++ test/test_arast_client.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export ARASTURL=140.221.84.124
+# export ARASTURL=140.221.84.124
 export PATH=/kb/deployment/bin:$PATH
 
 TEST_DIR="test_${$}"
@@ -32,21 +32,28 @@ rm -f smg.fa
 # curl -OL http://www.mcs.anl.gov/~fangfang/test/smg.fa
 wget http://www.mcs.anl.gov/~fangfang/test/smg.fa
 
+message "Login with Tester's ID"
+ar_login
+
 message "Submit synthetic  metagenome for kiki assembly and bwa mapping validation"
-export jobid=`arast -s $ARASTURL run -a kiki -f smg.fa --bwa`
+# export jobid=`arast -s $ARASTURL run -a kiki -f smg.fa --bwa`
+export jobid=`ar_run -a kiki -f smg.fa`
 echo "Job id = $jobid"
 
 message "Check job status"
 sleep 2
-arast -s $ARASTURL stat
+# arast -s $ARASTURL stat
+ar_stat
 sleep 5
 
 message "Check random data status"
-arast -s $ARASTURL stat --data 1
+# arast -s $ARASTURL stat --data 1
+ar_stat --data 1
 sleep 5
 
 message "Wait 90s for job to finish and download results"
 sleep 90
-arast -s $ARASTURL get -j $jobid
+# arast -s $ARASTURL get -j $jobid
+ar_get -j $jobid
 
 message "Tests Complete: PASSED"
