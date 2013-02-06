6fb72074bcd298244822647f5a98d9353f0370e1
Fangfang Xia <fangfang.xia@gmail.com>
2013-02-06 05:17:50 +0000

    attempt to fix httplib2 issue

--- lib/ar_client/auth_token.py
+++ lib/ar_client/auth_token.py
@@ -6,7 +6,7 @@ import base64
 NEXUS_URL="https://nexus.api.globusonline.org/goauth/token?grant_type=client_credentials"
 
 def get_token(username, password, auth_svc=NEXUS_URL):
-    h = httplib2.Http()
+    h = httplib2.Http(disable_ssl_certificate_validation=True)
     
     auth = base64.encodestring( username + ':' + password )
     headers = { 'Authorization' : 'Basic ' + auth }
@@ -19,6 +19,8 @@ def get_token(username, password, auth_svc=NEXUS_URL):
     status = int(resp['status'])
     if status>=200 and status<=299:
         tok = json.loads(content)
+    elif status == 403:
+        raise Exception( "Authentication failed: Bad user_id/password combination")
     else: 
         raise Exception(str(resp))
         
