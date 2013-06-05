ae4707d91093c4f8a36e2a9b937a9a180515c16d
Christopher Bun <chrisbun@gmail.com>
2013-06-05 17:59:09 +0000

    reapr

---
+++ lib/ar_server/plugins/reapr.py
@@ -0,0 +1,46 @@
+import copy
+import glob
+import logging
+import os
+import subprocess
+from plugins import BaseAssessment
+from yapsy.IPlugin import IPlugin
+
+class ReaprAssessment(BaseAssessment, IPlugin):
+    def run(self, contigs, reads):
+        """ 
+        Build the command and run.
+        Return list of file(s)
+        """
+        # Fix contig names with 'reapr facheck'
+        if len(contigs) > 1:
+            raise Exception('Reapr: multiple contig files!')
+        fixed_contig = os.path.join(self.outpath,
+                     os.path.basename(contigs[0]).rsplit('.', 1)[0] + '_fixed')
+        cmd_args = [self.executable, 'facheck', contigs[0], fixed_contig]
+        self.arast_popen(cmd_args)
+        fixed_contig = fixed_contig + '.fa'
+        if len(reads) > 1:
+            raise Exception('Reapr: multiple libraries not yet supported!')
+        readfiles = reads[0]['files']
+        
+        #Map with BWA and produce bam
+        bwa_data = copy.deepcopy(self.job_data)
+        bwa_data['initial_reads'][0]['files'] = readfiles
+        bwa_data['contigs'] = [fixed_contig]
+        bwa_data['out_report'] = open(os.path.join(self.outpath, 'bwa.log'), 'w')
+        samfile, _, _ = self.pmanager.run_module('bwa', bwa_data)
+        bamfile = samfile.replace('.sam', '.bam')
+        cmd_args = ['samtools', 'view',
+                    '-bSho', bamfile, samfile]
+        self.arast_popen(cmd_args)
+        sortedfile = os.path.join(self.outpath, 'sorted')
+        cmd_args = ['samtools', 'sort', bamfile, sortedfile]
+        self.arast_popen(cmd_args)
+        sortedfile = sortedfile + '.bam'
+        reapr_outdir = os.path.join(self.outpath, 'output')
+        cmd_args = [self.executable, 'pipeline', fixed_contig, sortedfile, 
+                    reapr_outdir]
+        self.arast_popen(cmd_args)
+        return []
+
