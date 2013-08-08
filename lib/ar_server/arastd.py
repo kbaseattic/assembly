44cda95cb2a43b41db3e6a1bc93231ed5cdffdce
Christopher Bun <chrisbun@gmail.com>
2013-08-08 15:46:53 +0000

    release notes, cleant arastd

--- lib/ar_server/arastd.py
+++ lib/ar_server/arastd.py
@@ -15,7 +15,6 @@ import os
 import pymongo
 import pika
 import router
-#import router2 as router
 from ConfigParser import SafeConfigParser
 
 import shock 
@@ -99,49 +98,50 @@ if args.verbose:
 else:
     logging.basicConfig(level=logging.DEBUG, filename=logfile)
 
-try:
-    os_user = os.environ.get('OS_AUTH_USER')
-    os_password = os.environ.get('OS_AUTH_KEY')
-    os_tenant = os.environ.get('OS_AUTH_TENANT')
-    os_auth_url = os.environ.get('OS_AUTH_URL')
-    cloud_control = True
-except:
-    print " [!] WARNING: Openstack environmental variables not set!  Disabling cloud monitor."
-    cloud_control = False
+# try:
+#     os_user = os.environ.get('OS_AUTH_USER')
+#     os_password = os.environ.get('OS_AUTH_KEY')
+#     os_tenant = os.environ.get('OS_AUTH_TENANT')
+#     os_auth_url = os.environ.get('OS_AUTH_URL')
+#     cloud_control = True
+# except:
+#     print " [!] WARNING: Openstack environmental variables not set!  Disabling cloud monitor."
+#     cloud_control = False
 
-if not os_user:
-    print " [!] WARNING: Openstack environmental variables not set!  Disabling cloud monitor."
-    cloud_control = False
+# if not os_user:
+#     print " [!] WARNING: Openstack environmental variables not set!  Disabling cloud monitor."
+#     cloud_control = False
 
-logging.info("OS_USER: %s" % os_user)
-logging.info("OS_TENANT: %s" % os_tenant)
+# logging.info("OS_USER: %s" % os_user)
+# logging.info("OS_TENANT: %s" % os_tenant)
 
 ########### DAEMON STUFF
-#if args.shock:
-#    shockurl = args.shock
-#if args.pidfile:
-#    print args.pidfile
-#    context = daemon.DaemonContext(stdout=sys.stdout, 
-#                                   pidfile=lockfile.FileLock(args.pidfile))
-#else:
-#    context = daemon.DaemonContext(stdout=sys.stdout)
-#context.signal_map = {
-#    signal.SIGTERM: tear_down,
-#    signal.SIGHUP: 'terminate'}
-#with context:
-#    start()
-##############
+# if args.pidfile:
+#     print args.pidfile
+#     context = daemon.DaemonContext(working_directory=os.getcwd(),
+#                                    stdout=sys.stdout, 
+#                                    pidfile=lockfile.FileLock(args.pidfile))
+# else:
+#     context = daemon.DaemonContext(stdout=sys.stdout)
+#     context.signal_map = {
+#     signal.SIGTERM: tear_down,
+#     signal.SIGHUP: 'terminate'}
+# with context:
+#     start(args.config, mongo_host=args.mongo_host, mongo_port=args.mongo_port,
+#           rabbit_host=args.rabbit_host, rabbit_port=args.rabbit_port, 
+#           deploy_config=args.deploy_config)
 
-if cloud_control:    
-    monitor = cloud.CloudMonitor(os_user, os_password, os_tenant, 
-                                 os_auth_url, args.config)
-    #monitor.list_ids()
-    #monitor.terminate_all_nodes()
-    #monitor.launch_node()
-    if monitor.list_nodes() is None:
-        #print ("No compute instances running. Launching...")
-        #monitor.launch_node()
-        pass
+# if cloud_control:    
+#     monitor = cloud.CloudMonitor(os_user, os_password, os_tenant, 
+#                                  os_auth_url, args.config)
+#     #monitor.list_ids()
+#     #monitor.terminate_all_nodes()
+#     #monitor.launch_node()
+#     if monitor.list_nodes() is None:
+#         #print ("No compute instances running. Launching...")
+#         #monitor.launch_node()
+#         pass
 
 start(args.config, mongo_host=args.mongo_host, mongo_port=args.mongo_port,
-      rabbit_host=args.rabbit_host, rabbit_port=args.rabbit_port, deploy_config=args.deploy_config)
+      rabbit_host=args.rabbit_host, rabbit_port=args.rabbit_port, 
+      deploy_config=args.deploy_config)
