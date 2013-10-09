89649e593cae704233ec83300ad017d0806f9307
Fangfang Xia <fangfang.xia@gmail.com>
2013-10-09 06:03:12 +0000

    minor update

--- lib/ar_client/client.py
+++ lib/ar_client/client.py
@@ -150,16 +150,24 @@ class Shock:
         #        '-X', 'POST', '-F', 'upload=@{}'.format(filename),
         #        '{}node/'.format(self.shockurl)]
 
-        cmd = ['curl','-X', 'POST', '-F', 'upload=@{}'.format(filename),
-               '{}node/'.format(self.shockurl)]
+        if "KB_RUNNING_IN_IRIS" in os.environ:
+            cmd = ['curl','-s','-X', 'POST', '-F', 'upload=@{}'.format(filename),
+                   '{}node/'.format(self.shockurl)]
+        else:
+            cmd = ['curl','-X', 'POST', '-F', 'upload=@{}'.format(filename),
+                   '{}node/'.format(self.shockurl)]
 
         ret = subprocess.check_output(cmd)
         res = json.loads(ret)
         return res
 
     def curl_download_file(self, node_id, outdir=None):
-        cmd = ['curl', '-H', 'Authorization: Globus-Goauthtoken {} '.format(self.token),
-               '-X', 'GET', '{}/node/{}'.format(self.shockurl, node_id)]
+        if "KB_RUNNING_IN_IRIS" in os.environ:
+            cmd = ['curl', '-s', '-H', 'Authorization: Globus-Goauthtoken {} '.format(self.token),
+                   '-X', 'GET', '{}/node/{}'.format(self.shockurl, node_id)]
+        else:
+            cmd = ['curl', '-H', 'Authorization: Globus-Goauthtoken {} '.format(self.token),
+                   '-X', 'GET', '{}/node/{}'.format(self.shockurl, node_id)]
         r = subprocess.check_output(cmd)
         filename = json.loads(r)['data']['file']['name']
         if outdir:
