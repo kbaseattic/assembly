967dc962a78e6e0cf7a6ecf9fb2b66f635cc452a
Fangfang Xia <fangfang.xia@gmail.com>
2013-12-12 23:08:30 +0000

    minor update

--- lib/ar_server/plugins/pacbio.py
+++ lib/ar_server/plugins/pacbio.py
@@ -12,18 +12,24 @@ class PacbioAssembler(BaseAssembler, IPlugin):
         Return list of contig file(s)
         """
         
-        self.get_smrt_env()
+        smrt_env = self.get_smrt_env()
 
         cmd_args = [self.executable, '-help']
         logging.info("Running subprocess:{}".format(cmd_args))
+
         print " ".join(cmd_args)
-        self.arast_popen(cmd_args)        
+
+        self.arast_popen(cmd_args, env=smrt_env)
 
 
     def get_smrt_env(self):
         source_cmd = " ".join(['source', self.setup, ';', 'printenv' ]);
         proc = subprocess.Popen([source_cmd], stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
         lines = proc.stdout.readlines()
-        print lines
+        env = os.environ.copy()
+        for line in lines:
+            k, v = line.split('=', 1)
+            env[k] = v
+        print env
         
         
