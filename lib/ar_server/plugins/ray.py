7b07985c02135eecb1822b3ef7fd1436d5354e05
Fangfang Xia <fangfang.xia@gmail.com>
2014-01-03 21:21:21 +0000

    add ray plugin

---
+++ lib/ar_server/plugins/ray.py
@@ -0,0 +1,35 @@
+import glob
+import logging
+import os
+import subprocess
+from plugins import BaseAssembler
+from yapsy.IPlugin import IPlugin
+
+class RayAssembler(BaseAssembler, IPlugin):
+    def run(self, reads):
+        """ 
+        Build the command and run.
+        Return list of contig file(s)
+        """
+        
+        cmd_args = ["mpirun", "-np", "4", self.executable, '-k', self.k]
+        
+        for lib in reads:
+            if lib['type'] == 'paired':
+                if len(lib['files']) == 1: # Interleaved
+                    cmd_args += ['-i', lib['files'][0]]
+                elif len(lib['files']) == 2: # 2 Files
+                    cmd_args += ['-p', lib['files'][0], lib['files'][1]]
+                else:
+                    raise Exception('Ray module file error')
+            elif lib['type'] == 'single':
+                cmd_args += ['-s', lib['files'][0]]
+
+        cmd_args += ['-o', self.outpath + '/RayOutput']
+
+        self.arast_popen(cmd_args)
+
+        contigs = os.path.join(self.outpath+'/RayOutput', 'Scaffolds.fasta')
+
+        return contigs
+
