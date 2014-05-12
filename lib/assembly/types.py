feaefe5b0f6cb9c08f03bd19c6d04b0dfadc18fe
Christopher Bun <chrisbun@gmail.com>
2014-05-12 17:48:38 +0000

    types.py

---
+++ lib/assembly/types.py
@@ -0,0 +1,22 @@
+class FileInfo(dict):
+    def __init__(self, filename, shock_url=None, shock_id=None, 
+                 filesize=None, create_time=None, metadata=None, *args):
+        dict.__init__(self, *args)
+        self.update({'shock_url': shock_url,
+                     'shock_id' : shock_id,
+                     'filesize': filesize,
+                     'filename': filename,
+                     'create_time': create_time,
+                     'metadata': metadata})
+
+class FileSet(dict):
+    def __init__(self, set_type, file_infos, 
+                 **kwargs):
+        dict.__init__(self, **kwargs)
+        self.update({'type': set_type,
+                     'file_infos': []})
+        if type(file_infos) is list:
+            for f in file_infos:
+                self['file_infos'].append(f)
+        else:
+            self['file_infos'] = [file_infos]
