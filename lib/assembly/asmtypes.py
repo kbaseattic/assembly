faae534e6ceaa0ca603cbc92c7dfc5406b65c3de
Christopher Bun <chrisbun@gmail.com>
2014-05-15 00:37:06 +0000

    New types

--- lib/assembly/asmtypes.py
+++ lib/assembly/asmtypes.py
@@ -1,3 +1,6 @@
+import os
+
+#### Single Files #####
 class FileInfo(dict):
     def __init__(self, filename, shock_url=None, shock_id=None, name=None,
                  filesize=None, create_time=None, metadata=None, *args):
@@ -5,10 +8,15 @@ class FileInfo(dict):
         self.update({'shock_url': shock_url,
                      'shock_id' : shock_id,
                      'filesize': filesize,
-                     'filename': filename,
+                     'filename': os.path.basename(filename),
+                     'local_file': filename,
                      'create_time': create_time,
                      'metadata': metadata})
 
+
+
+
+##### Set of Files ######
 class FileSet(dict):
     def __init__(self, set_type, file_infos, 
                  **kwargs):
@@ -20,4 +28,63 @@ class FileSet(dict):
                 self['file_infos'].append(f)
         else:
             self['file_infos'] = [file_infos]
+
+    @property
+    def files(self):
+        """ Returns file paths of all files in set"""
+        return [f['local_file'] for f in self['file_infos']]
+
+
+class ReadSet(FileSet):
+    def __init__(self, set_type, file_infos,  **kwargs):
+        FileSet.__init__(self, set_type, file_infos, **kwargs)
+        self.__dict__.update(kwargs)
+
+    @property
+    def insert_len(self):
+        return self.insert or None
+
+def set_factory(set_type, file_infos, **kwargs):
+    if set_type in ['paired', 'single']:
+        return ReadSet(set_type, file_infos, **kwargs)
+    else:
+        return FileSet(set_type, file_infos, **kwargs)
+
+
+#### All Filesets #####
+class FileSetContainer(dict):
+    def __init__(self, filesets=None):
+        self.filesets = filesets if filesets else []
+
+    @property
+    def readsets(self):
+        return [fileset for fileset in self.filesets if type(fileset) is ReadSet]
+
+    @property
+    def readsets_paired(self):
+        return [readset for readset in self.readsets if readset['type'] == 'paired']
+
+    @property
+    def readsets_single(self):
+        return [readset for readset in self.readsets if readset['type'] == 'single']
+    
+    @property
+    def contigs(self):
+        pass
+
+    @property
+    def reference(self):
+        pass
+
+    @property
+    def bamfiles(self):
+        pass
+
+
+def filepaths(filesets):
+    """ Return a list of filepaths from list of FileSets """
+    filepaths = []
+    for fs in filesets:
+        filepaths += fs.files
+    return filepaths
 
