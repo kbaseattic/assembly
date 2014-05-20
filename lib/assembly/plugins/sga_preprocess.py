2d7d6109b113f6699a9d87cd0ab0256b1571a023
Chris Bun <chrisbun@gmail.com>
2014-05-20 22:04:22 +0000

    update sga plugin

--- lib/assembly/plugins/sga_preprocess.py
+++ lib/assembly/plugins/sga_preprocess.py
@@ -7,42 +7,36 @@ from yapsy.IPlugin import IPlugin
 from assembly import get_qual_encoding
 
 class SgaPreprocessor(BasePreprocessor, IPlugin):
-    def run(self, reads):
+    new_version = True
+
+    def run(self, reads=None):
         """ 
         Build the command and run.
         Return list of reads
         """
         processed_reads = []
-        print reads
-        for file_set in reads: # preprocess all pairs/reads!!!
-            new_file_set = dict(file_set)
-            new_file_set['files'] = []
-            for f in file_set['files']:
-                cmd_args = [os.path.join(os.getcwd(), self.executable), 'preprocess']
-                if self.quality_trim:
-                    cmd_args += ['-q', self.quality_trim]
-                if self.quality_filter:
-                    cmd_args += ['-f', self.quality_filter]
-                if self.min_length:
-                    cmd_args += ['-m', self.min_length]
-                if self.permute_ambiguous:
-                    cmd_args.append('--permute-ambiguous')
-                files = file_set['files']                    
+        for f in self.data.readfiles:
+            cmd_args = [os.path.join(os.getcwd(), self.executable), 'preprocess']
+            if self.quality_trim:
+                cmd_args += ['-q', self.quality_trim]
+            if self.quality_filter:
+                cmd_args += ['-f', self.quality_filter]
+            if self.min_length:
+                cmd_args += ['-m', self.min_length]
+            if self.permute_ambiguous:
+                cmd_args.append('--permute-ambiguous')
 
-                # Get file name for prefix
-                cmd_args.append('-o')
-                basename = os.path.basename(f)
-                base = basename[0:basename.rfind('.')]
-                #base = os.path.basename(f).split('.')[0]
-                pp_file = os.path.join(self.outpath,
-                                       "{}.pp.fastq".format(base))
-                cmd_args.append(pp_file)
-                cmd_args.append(f)
-                if get_qual_encoding(files[0]) == 'phred64':
-                    cmd_args.append('--phred64')
-                logging.info("SGA Plugin: {}".format(cmd_args))
-                self.arast_popen(cmd_args, cwd=self.outpath)
-                new_file_set['files'].append(pp_file)
-            processed_reads.append(new_file_set)
-        return processed_reads
+            # Get file name for prefix
+            cmd_args.append('-o')
+            basename = os.path.basename(f)
+            base = basename[0:basename.rfind('.')]
+            pp_file = os.path.join(self.outpath,
+                                   "{}.pp.fastq".format(base))
+            cmd_args.append(pp_file)
+            cmd_args.append(f)
+            if get_qual_encoding(f) == 'phred64':
+                cmd_args.append('--phred64')
+            self.arast_popen(cmd_args, cwd=self.outpath)
+            processed_reads.append(pp_file)
+        return {'reads': processed_reads}
 
