7ad448748c07104b8fc7aaaa314e9bdab1bbefc1
Chris Bun <chrisbun@gmail.com>
2014-05-27 22:00:21 +0000

    rewrite GAM to use Wasp

--- lib/assembly/plugins/gam_ngs.py
+++ lib/assembly/plugins/gam_ngs.py
@@ -8,40 +8,33 @@ from plugins import BaseMetaAssembler
 from yapsy.IPlugin import IPlugin
 
 class GamNgsAssembler(BaseMetaAssembler, IPlugin):
+    new_version = True
+
     def run(self, contigs=None, type='in_order'):
         """ 
         Other types: iterative, smart
         """
 
-        contigs = self.data.contigsets
-
         libfiles = []
+        contigs = self.data.contigfiles
+        contigsets = self.data.contigsets
         if len(contigs) < 2:
             return []
-        for data in contigs:
-            libfile = os.path.join(self.outpath, data['name'] + '.txt')
+        for cset in contigsets:
+            libfile = os.path.join(self.outpath, cset.name + '.lib')
             lib = open(libfile, 'w')
-
-            #Map with BWA and produce bam
-            bwa_data = copy.deepcopy(self.job_data)
-            bwa_data['contigs'] = data.files
-            bwa_data['out_report'] = open(os.path.join(self.outpath, 'bwa.log'), 'w')
-            samfiles, _, _ = self.pmanager.run_module('bwa', bwa_data)
-
-
-
-
-            for samfile in samfiles:
-                #insert, stdev = self.estimate_insert_stdev()
+            bwa_results = self.plugin_engine.run_expression('(bwa (contigs {}) READS)'.format(cset.files[0]))
+            for samfile in bwa_results.files:
                 bamfile = samfile.replace('.sam', '.bam')
                 cmd_args = ['samtools', 'view',
                             '-bSho', bamfile, samfile]
                 self.arast_popen(cmd_args, overrides=False)
-                sortedfile = os.path.join(self.outpath, data['name'])
+                sortedfile = os.path.join(self.outpath, cset.name)
                 cmd_args = ['samtools', 'sort', bamfile, sortedfile]
                 self.arast_popen(cmd_args, overrides=False)
                 undupfile = sortedfile + '_undup.bam'
                 sortedfile = sortedfile + '.bam'
+
                 ## Remove duplicates
                 cmd_args = ['samtools', 'rmdup', sortedfile, undupfile]
                 self.arast_popen(cmd_args, overrides=False)
@@ -51,11 +44,10 @@ class GamNgsAssembler(BaseMetaAssembler, IPlugin):
                 cmd_args = ['samtools', 'index', undupfile]
                 self.arast_popen(cmd_args, overrides=False)
 
-                #data['alignment_bam'].append(undupfile)
                 lib.write('{}\n'.format(undupfile))
                 lib.write('50 10000\n')
             lib.close()
-            libfiles.append({'file': libfile, 'name': data['name'], 'contigs': data['files'][0]})
+            libfiles.append({'file': libfile, 'name': cset.name, 'contigs': cset.files[0]})
             
         merged_files = []
 
@@ -68,7 +60,7 @@ class GamNgsAssembler(BaseMetaAssembler, IPlugin):
                 if mfile:
                     merged_files.append(mfile)
         elif type == 'in_order':
-            mfile = self.merge(pair[0], pair[1])
+            mfile = self.merge(libfiles[0], libfiles[1])
             if mfile:
                 merged_files.append(mfile)
         #elif type == 'iterative':
