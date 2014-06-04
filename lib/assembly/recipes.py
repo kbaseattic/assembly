f4f68d1f485486c54442eeb4db7d79f6fb678e64
Chris Bun <chrisbun@gmail.com>
2014-06-04 21:52:03 +0000

    New Auto recipe

--- lib/assembly/recipes.py
+++ lib/assembly/recipes.py
@@ -1,4 +1,4 @@
-auto = """
+auto2 = """
 (begin
  (define pp (bhammer (sga_preprocess READS)))
  (define sp (spades pp))
@@ -8,9 +8,17 @@ auto = """
 )
 """
 
+auto = """
+(begin
+ (define pp READS)
+ (define kval (get best_k (kmergenie pp)))
+ (define vt (begin (setparam hash_length kval) (velvet pp)))
+ (define sp (spades pp))
+ (define id (idba pp))
+ (define gam (gam_ngs (slice (sort (list sp id vt) > :key (lambda (c) (n50 c))) 0 2)))
+ (quast (emit (sspace pp gam)) (emit (get scaffolds sp)) (emit (get scaffolds id)) (emit (sspace pp vt)))
+)
 
-
-
-jigsaw = '''  '''
+"""
 
 
