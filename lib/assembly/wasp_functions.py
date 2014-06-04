4072c5e0d185c43e9c0527e5b9dbec7c7d3b0022
Chris Bun <chrisbun@gmail.com>
2014-06-04 21:34:04 +0000

    n50 function

--- lib/assembly/wasp_functions.py
+++ lib/assembly/wasp_functions.py
@@ -1,5 +1,6 @@
 import asmtypes
 import wasp
+from Bio import SeqIO
 
 ####### Decorators
 def wasp_contigs(func):
@@ -9,9 +10,15 @@ def wasp_contigs(func):
         for w in wasplinks:
             contigs += w['default_output'].files
         newlink = wasp.WaspLink()
-        newlink['default_output'] = asmtypes.set_factory('contigs', [func(contigs)])
+        output = func(contigs)
+        if type(output) is int:
+            return output
+        newlink['default_output'] = asmtypes.set_factory('contigs', [output])
         return newlink
     return func_on_contigs
+
+##### FileSet Functions #####
+## sorting/filtering
 
 
 ###### Raw data Functions
@@ -19,8 +26,30 @@ def wasp_contigs(func):
 def best_contig(contigs):
     return contigs[0]
 
+@wasp_contigs
 def n50(contigs):
-    return 42
+    ''' https://code.google.com/p/biopyscripts '''
+    contig = contigs[0]
+    contigsLength = []
+    sum = 0
+    for seq_record in SeqIO.parse(open(contig), "fasta"):
+        sum += len(seq_record.seq)
+        contigsLength.append(len(seq_record.seq))
+
+    teoN50 = sum / 2.0     
+    contigsLength.sort()   
+    contigsLength.reverse()    
+
+    #checking N50
+    testSum = 0
+    N50 = 0
+    for con in contigsLength:
+        testSum += con
+        if teoN50 < testSum:
+            N50 = con
+            break
+    print 'N50: ' + str(N50)  
+    return N50
 
 ####### Wasp expressions
 
