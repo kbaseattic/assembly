da882a9195c014d07c4ea73498a54bb93835c198
Fangfang Xia <fangfang.xia@gmail.com>
2014-06-26 22:37:40 -0500

    add --version option

--- client/ar-stat.pl
+++ client/ar-stat.pl
@@ -7,24 +7,28 @@ Getopt::Long::Configure("pass_through");
 
 my $usage = <<End_of_Usage;
 
-Usage: ar-stat [-h] [-w] [-d [FILES] | -j STAT_JOB] [-n STAT_N] [-s server_addr]
+Usage: ar-stat [-h] [-w] [-j JOB | -l | --data-json DATA] [-n STAT_N] [-s server_addr]
 
 Query status of running jobs
 
 Optional arguments:
   -h, --help            show this help message and exit
-  -j JOB, --job JOB     get status of specific job
-  -n STAT_N             specify number of records to show
+  -j JOB, --job JOB     get status of a specific job ID
+  -l, --list-data       list data objects
+  --data-json DATA      print the information of a data ID
   -s server_addr        specify ARAST server address
   -w, --watch           monitor in realtime (only works in Linux and Mac shell)
+  --version             print ARAST client version number
 
 End_of_Usage
 
 my $help;
 my $server;
+my $version;
 
-my $rc = GetOptions("h|help" => \$help,
-                    "s=s" => \$server);
+my $rc = GetOptions("h|help"  => \$help,
+                    "s=s"     => \$server,
+                    "version" => \$version);
 
 $rc or die $usage;
 if ($help) {
@@ -35,5 +39,9 @@ if ($help) {
 my $arast = 'arast';
 $arast .= " -s $server" if $server;
 
-!system "$arast stat @ARGV" or die $!;
+if ($version) {
+    system "$arast --version"; 
+} else {
+    !system "$arast stat @ARGV" or die $!."\n";
+}
                     
