7ae9f2f9d8ad076a8e2c31c00b7b5455f3e2513b
Christopher Bun <chrisbun@gmail.com>
2014-06-27 01:22:02 +0000

    Fix deploy script for file restructure

--- tools/deploy-egg.sh
+++ tools/deploy-egg.sh
@@ -6,10 +6,11 @@ cp setup.py egg
 cp ../lib/assembly/auth_token.py egg/assembly/
 cp ../lib/assembly/asmtypes.py egg/assembly/
 cp ../lib/assembly/client.py egg/assembly/
+cp ../lib/assembly/kbase.py egg/assembly/
 cp ../lib/assembly/config.py egg/assembly/
 cp ../lib/assembly/shock.py egg/assembly/
 cp ../lib/assembly/__init__.py egg/assembly/
 
-cp arast.py egg/assembly
+cp ../client/arast.py egg/assembly
 cd egg
 python setup.py bdist_egg
