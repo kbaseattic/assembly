71ae9a925f48a8b70d462d799e42360383feac65
Christopher Bun <chrisbun@gmail.com>
2014-07-01 15:32:04 +0000

    minor

--- lib/assembly/router.py
+++ lib/assembly/router.py
@@ -37,7 +37,7 @@ def send_message(body, routingKey):
             host=rmq_host))
     channel = connection.channel()
     channel.queue_declare(queue=routingKey, durable=True)
-    #channel.basic_qos(prefetch_count=1)
+    channel.basic_qos(prefetch_count=1)
     channel.basic_publish(exchange = '',
                           routing_key=routingKey,
                           body=body,
@@ -78,11 +78,10 @@ def get_upload_url():
     global parser
     return parser.get('shock', 'host')
 
-
 def check_valid_client(body):
     client_params = json.loads(body) #dict of params
     min_version = parser.get('assembly', 'min_cli_version')
-    try: return StrictVersion(client_params['version']) >= StrictVersion(min_version):
+    try: return StrictVersion(client_params['version']) >= StrictVersion(min_version)
     except: return True
 
 def route_job(body):
