a10433b23160f50f7df98fe10d59c15f8d9bf939
Fangfang Xia <fangfang@mcs.anl.gov>
2014-07-02 14:09:15 -0500

    minor update

--- client/arast.py
+++ client/arast.py
@@ -351,7 +351,7 @@ def main():
             try:
                 report = aclient.get_job_report(args.job)
             except Exception as e:
-                sys.exit("Error retrieving job report: " + e)
+                sys.exit("Error retrieving job report: {}".format(e))
             if report:
                 print report
 
