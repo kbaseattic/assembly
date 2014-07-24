87d3dbb44d5d94438cbd710a96777f254d590116
Christopher Bun <chrisbun@gmail.com>
2014-07-24 21:45:27 +0000

    bowtie2: merge SAMs on mixed reads

--- lib/assembly/plugins/bowtie2.py
+++ lib/assembly/plugins/bowtie2.py
@@ -16,23 +16,49 @@ class Bowtie2Aligner(BaseAligner, IPlugin):
         contig_file = self.data.contigfiles[0]
         reads = self.data.readfiles
 
-        ## Index contigs using IS algorithm
+        ## Index contigs 
         prefix = os.path.join(self.outpath, 'bt2')
         cmd_args = [self.build_bin, '-f', contig_file, prefix]
         self.arast_popen(cmd_args, overrides=False)
 
-        ## Align reads
-        samfile = os.path.join(self.outpath, 'align.sam')
-        cmd_args = [self.executable, '-x', prefix, '-S', samfile,
-                    '-p', self.process_threads_allowed]
-        if len(reads) == 2:
-            cmd_args += ['-1', reads[0], '-2', reads[1]]
-        elif len(reads) == 1:
-            cmd_args += ['-U', reads[0]]
-        else:
-            raise Exception('Bowtie plugin error')
-        self.arast_popen(cmd_args, overrides=False)
+        ### Align reads
+        bamfiles = []
+        for i, readset in enumerate(self.data.readsets):
+            samfile = os.path.join(self.outpath, 'align.sam')
+            reads = readset.files
+            cmd_args = [self.executable, '-x', prefix, '-S', samfile,
+                        '-p', self.process_threads_allowed]
+            if len(reads) == 2:
+                cmd_args += ['-1', reads[0], '-2', reads[1]]
+            elif len(reads) == 1:
+                cmd_args += ['-U', reads[0]]
+            else:
+                raise Exception('Bowtie plugin error')
+            self.arast_popen(cmd_args, overrides=False)
+            if not os.path.exists(samfile):
+                raise Exception('Unable to complete alignment')
 
-        if not os.path.exists(samfile):
-            raise Exception('Unable to complete alignment')
-        return {'alignment': samfile}
+            ## Convert to BAM
+            bamfile = samfile.replace('.sam', '.bam')
+            cmd_args = ['samtools', 'view',
+                        '-bSho', bamfile, samfile]
+            self.arast_popen(cmd_args)
+            bamfiles.append(bamfile)
+
+        ### Merge samfiles if multiple
+        if len(bamfiles) > 1:
+            bamfile = os.path.join(self.outpath, '{}_{}.bam'.format(os.path.basename(contig_file), i))
+            self.arast_popen(['samtools', 'merge', bamfile] + bamfiles)
+            if not os.path.exists(bamfile):
+                raise Exception('Unable to complete alignment')
+        else:
+            bamfile = bamfiles[0]
+            if not os.path.exists(bamfile):
+                raise Exception('Unable to complete alignment')
+
+        ## Convert back to sam    
+        samfile = bamfile.replace('.bam', '.sam')
+        self.arast_popen(['samtools', 'view', '-h', '-o', samfile, bamfile])
+
+        return {'alignment': samfile,
+                'alignment_bam': bamfile}
