26ddefd6b83e43dbf5b4389fe1355fc98e9d3e32
Christopher Bun <chrisbun@gmail.com>
2014-08-16 18:05:06 +0000

    deploy wheel script

---
+++ tools/deploy-wheel.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+mkdir wheel
+mkdir wheel/assembly
+cp setup.py wheel
+cp ../lib/assembly/auth.py wheel/assembly/
+cp ../lib/assembly/asmtypes.py wheel/assembly/
+cp ../lib/assembly/client.py wheel/assembly/
+cp ../lib/assembly/config.py wheel/assembly/
+cp ../lib/assembly/kbase.py wheel/assembly/
+cp ../lib/assembly/shock.py wheel/assembly/
+cp ../lib/assembly/__init__.py wheel/assembly/
+
+cp ../client/arast.py wheel/assembly
+cd wheel
+python setup.py bdist_wheel --universal
