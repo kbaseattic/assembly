e635c25536047251642abcdc3cf2d7c8f859e654
Christopher Bun <chrisbun@gmail.com>
2014-08-18 21:07:39 +0000

    client egg/wheel build tag

--- tools/setup.py
+++ tools/setup.py
@@ -1,8 +1,17 @@
+import shlex
 import os
 from setuptools import setup, find_packages
+from subprocess import check_output
 from assembly import __version__
 
 CLIENT_VERSION = __version__
+GIT_HEAD_REV = check_output(shlex.split('git rev-parse --short HEAD')).strip()
+STABLE_VERSION = False
+
+if STABLE_VERSION:
+    tag_build = "stable"
+else:
+    tag_build = "dev_" + GIT_HEAD_REV
 
 # Utility function to read the README file.
 def read(fname):
@@ -15,6 +24,7 @@ setup(
     author_email = "chrisbun@gmail.com",
     description = ("A commandline client for the KBASE Assembly Service"),
     url = "http://www.kbase.us/services/assembly",
+    options = dict(egg_info = dict(tag_build = tag_build)),
     packages = find_packages(),
     install_requires = ['requests>=2.1.0', 'httplib2', 'appdirs', 'prettytable'],
     entry_points={'console_scripts':[
