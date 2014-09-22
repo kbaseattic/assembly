5a389b8d79ab8f3d82b00e487cb9ab32288920d6
Christopher Bun <chrisbun@gmail.com>
2014-09-22 22:26:36 +0000

    perl client dependencies

--- tools/install-basic-dependencies.sh
+++ tools/install-basic-dependencies.sh
@@ -2,10 +2,14 @@
 
 sudo apt-get -y install cpanminus cmake
 
+sudo cpanm install Config::Simple
 sudo cpanm install Cwd
 sudo cpanm install Data::Dumper
+sudo cpanm install DateTime
 sudo cpanm install File::Basename
 sudo cpanm install File::Copy
 sudo cpanm install Getopt::Long
+sudo cpanm install Term::ReadKey
+sudo cpanm install Text::Table
 
 sudo pip install pika python-daemon pymongo requests yapsy numpy biopython
