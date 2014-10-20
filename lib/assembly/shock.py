07916e8528a25ba521655118d00e518200b85b6c
Christopher Bun <chrisbun@gmail.com>
2014-10-20 20:47:12 +0000

    more curl headers

--- lib/assembly/shock.py
+++ lib/assembly/shock.py
@@ -147,7 +147,10 @@ class Shock:
         cmd = ['curl', 
                '-o', filename, d_url]
 
-        p = subprocess.Popen(cmd, cwd=outdir)
+        for k,v in self.headers.items():
+            cmd += ['-H', '"{}: OAuth {}"'.format(k,v)]
+
+        p = subprocess.Popen(' '.join(cmd), cwd=outdir, shell=True)
         p.wait()
 
         downloaded = os.path.join(outdir, filename)
