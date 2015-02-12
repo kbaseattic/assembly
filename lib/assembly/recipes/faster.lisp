642289917bf5661254d05d5a66df8219b77c986e
Fangfang Xia <fangfang@cs.uchicago.edu>
2015-02-12 22:25:35 +0000

    use MEGAHIT in two recipes: fast & faster

--- lib/assembly/recipes/faster.lisp
+++ lib/assembly/recipes/faster.lisp
@@ -1,13 +1,10 @@
-;;; Assembles with A6 and Velvet.
+;;; Assembles with Velvet and MEGAHIT.
 ;;; Results are sorted by ARAST quality Score.
 ;;; Works for some short read datasets.
 (begin
   (define vt (velvet READS))
-  (if (has_paired READS)
-    (prog
-      (define aa (a6 READS))
-      (define assemblies (list vt aa)))
-    (define assemblies (list vt)))
+  (define mh (megahit READS))
+  (define assemblies (list vt mh))
   (define newsort (sort assemblies > :key (lambda (c) (arast_score c))))
   (tar (all_files (quast (upload newsort))) :name analysis)
 )
