4f3674356a2c31759119021840fb207a273274d6
Fangfang Xia <fangfang@cs.uchicago.edu>
2015-03-08 15:48:59 +0000

    refactor logging in plugins

--- lib/assembly/plugins.py
+++ lib/assembly/plugins.py
@@ -27,6 +27,7 @@ import wasp
 
 logger = logging.getLogger(__name__)
 
+
 class BasePlugin(object):
     """
     job_data dictionary must contain:
@@ -97,8 +98,8 @@ class BasePlugin(object):
         self.out_module.write("Command: {}\n".format(cmd_string))
         try:
             self.out_report.write('Command: {}\n'.format(cmd_string))
-        except:
-            logger.error('Could not write to report: {}'.format(cmd_string))
+        except Exception as e:
+            logger.error('Could not write to report: {} -- {}'.format(cmd_string, e))
         m_start_time = time.time()
         # print "Command args: {}".format(cmd_args)
         logger.info("Command line: {}".format(cmd_string if shell else " ".join(cmd_args)))
@@ -145,12 +146,14 @@ class BasePlugin(object):
                     self.out_module.write(line)
 
         except subprocess.CalledProcessError as e:
-            out = 'Process Failed.\nExit Code: {}\nOutput:{}\n'.format(
-                e.returncode, e.output)
+            logger.warn('Process Failed.\nExit Code: {}\nOutput:{}\n'.format(e.returncode, e.output))
+
         m_elapsed_time = time.time() - m_start_time
         m_ftime = str(datetime.timedelta(seconds=int(m_elapsed_time)))
-        try: self.out_report.write('Command: {}\n'.format(m_ftime))
-        except: print 'Could not write to report: {}'.format(cmd_string)
+        try:
+            self.out_report.write('Command: {}\n'.format(m_ftime))
+        except Exception as e:
+            logger.error('Could not write to report: {} -- {}'.format(cmd_string, e))
 
     def is_urgent_output(self, line):
         """
@@ -490,7 +493,8 @@ class BasePreprocessor(BasePlugin):
             for extra in output['extra']:
                 readsets.append(asmtypes.set_factory('single', extra,
                                                      name='{}_single'.format(self.name)))
-        except Exception as e: print e
+        except Exception as e:
+            logger.warn('Wasp handles extra output: {}'.format(e))
         return {'reads': readsets}
 
     # Must implement run() method
