6d71b2f457b6dbe474fce9dbfcf240fb23a2b527
Fangfang Xia <fangfang@anl.gov>
2015-06-11 17:43:28 -0500

    minor update

--- env/server-env-for-p3-worker.sh
+++ env/server-env-for-p3-worker.sh
@@ -1,5 +1,5 @@
 
-export ARAST_TOP=/home/ubuntu/arast.p3
+ARAST_TOP=${VAR_DIR:-/home/ubuntu/arast.p3}
 
 export ARAST_URL=http://tutorial.theseed.org/assembly
 export ARAST_CONFIG=$ARAST_TOP/mnt/assembly/lib/assembly/arast.p3.conf
